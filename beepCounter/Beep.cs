06ce0735200eee2fed737a42120b3f7143685f94
Leandro Marson Ribeiro <leandromarson1@hotmail.com>
2020-08-24 18:02:34 -0300

    beep

---
+++ beepCounter/Beep.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading;
+
+namespace beepCounter
+{
+
+    public class Beep
+    {
+
+        Int32 len = 32767;
+
+        public void play()
+        {
+
+            try
+            {
+                System.Console.WriteLine("Begin\n");
+                System.Threading.Thread.Sleep(1000);
+                for (Int32 i = 37; i < len; i++)
+                {
+                    System.Console.WriteLine($"Frequency: {i}");
+                    Console.Beep(i, 1000);
+                }
+            }
+            catch (System.Exception e)
+            {
+                System.Console.WriteLine(e);
+                throw;
+            }
+
+        }
+
+    }
+}
---
+++ beepCounter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+
+namespace beepCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beep b = new Beep();
+
+            b.play();
+
+            Console.ReadKey();
+        }
+    }
+}
