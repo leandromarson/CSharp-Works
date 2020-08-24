e2eaa1044867fe8cf964c99fa0635d180691216b
Leandro Marson Ribeiro <leandromarson1@hotmail.com>
2020-08-24 17:17:35 -0300

    v

--- maxmin/Program.cs
+++ maxmin/Program.cs
@@ -7,14 +7,19 @@ namespace maxmin
         static void Main(string[] args)
         {
             Char rangeChar = new Char();
+
             SByte rangeSByte = new SByte();
             Byte rangeByte = new Byte();
+
             Short rangeShort = new Short();
             UShort rangeUShort = new UShort();
+
             Int rangeInt = new Int();
             UInt rangeUInt = new UInt();
+
             Long rangeLong = new Long();
             ULong rangeULong = new ULong();
+
             Float rangeFloat = new Float();
             Double rangeDouble = new Double();
             Decimal rangeDecimal = new Decimal();
@@ -31,7 +36,7 @@ namespace maxmin
             rangeFloat.rPrint();
             rangeDouble.rPrint();
             rangeDecimal.rPrint();
-            
+
             Console.ReadKey();
 
         }
